f4eb78d1c8414c28640b1041b0dc9932ada6eecf
Suryamani <scsuryamani@gmail.com>
2020-10-06 18:05:39 +0530

    added host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!p1ai_b373g3bmz-**m@%h9+0_8xm7*41etdbi+t266-mogm08'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['safetourism.herokuapp.com','*','localhost',' http://127.0.0.1:8000/',]
+ALLOWED_HOSTS = ['safetourism.herokuapp.com','*','localhost',' http://127.0.0.1:8000/','http://localhost:3000']
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # During development only
