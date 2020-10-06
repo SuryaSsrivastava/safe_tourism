afcd6903b3f9c470b002510c72fe28078e1c79a3
Suryamani <scsuryamani@gmail.com>
2020-10-06 21:19:03 +0530

    bugs fixed

--- tourist/api/urls.py
+++ tourist/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from tourist.api.views import(
-	tourist_view,all_tourist_places,
+	tourist_view,all_tourist_places,particular_place
 )
 
 
@@ -11,5 +11,6 @@ urlpatterns = [
 	path('tourist_place/', tourist_view, name="tourist_place"), #Api end point to add tourist places(POST request)
     path('all_tourist_places/', all_tourist_places, name="all_tourist_places"), #Api end point to GET all tourist places from database
 	# path('booking/', booking_view, name="booking"), #Api end point to book any tourist places(POST/PUT request)
+	path('all_tourist_places/<int:place_id>', particular_place, name="particular_place"),
 ]
 
