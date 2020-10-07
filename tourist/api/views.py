0822db3cdb06b756cd3dd0d7dc93b10c85911044
Suryamani <scsuryamani@gmail.com>
2020-10-07 08:41:07 +0530

    bugs fixed

--- tourist/api/views.py
+++ tourist/api/views.py
@@ -27,59 +27,6 @@ def all_tourist_places(request):
     d['data'] = j
     for i in queryset.values():
         d['data'].append(i)
-    #corona Api
-
-    # url = "https://coronavirus-19-api.herokuapp.com/countries"
-    # data = requests.get(url)
-    # serverError = False
-    # try:
-    #     data = data.json()
-    # except :
-    #     serverError = {"message" : 'server_error'}
-    #     return JsonResponse(serverError)
-    # cases,todayCases,deaths,recovered =0,0,0,0
-    # error = False
-    # country = "Global"
-    
-    # if request.method == "POST" and not request.POST["country"]=="":
-    #     for x in data:
-    #         if x.get("country").lower()== request.POST["country"].lower():
-    #             cases += x["cases"]
-    #             todayCases += x["todayCases"] 
-    #             deaths += x["deaths"]
-    #             recovered += x["recovered"]
-    #             country = x["country"]
-    #             error = False
-    #             break
-    #         else:
-    #             error = True
-    #             continue
-    # else:
-    #     for x in data:
-    #         error = False
-    #         try:
-    #             todayCases += x["todayCases"]
-    #         except:
-    #             todayCases +=0
-    #         try:
-    #             cases += x["cases"]
-    #         except:
-    #             cases +=0
-    #         try:
-    #             deaths += x["deaths"]
-    #         except:
-    #             deaths +=0
-    #         try:
-    #             recovered += x["recovered"]
-    #         except:
-    #             recovered +=0
-    # if error : 
-    #     data = {"error":error,"cases" : cases,"todayCases":todayCases,"deaths":deaths,"recovered":recovered,"country":country}
-    # else :
-    #     data = {"cases" : cases,"todayCases":todayCases,"deaths":deaths,"recovered":recovered,"country":country}
-
-
-
     return JsonResponse(d)
 
 @api_view(['GET'])
